7542b1a70d1a51aa510bdc63aa14cb1dfb7554a7
antoonioaraujo <ajaraujoplata@gmail.com>
2024-04-24 11:30:54 +0200

    sexooo

--- src/TFG/Peliculas.java
+++ src/TFG/Peliculas.java
@@ -5,6 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JScrollBar;
+import java.awt.Choice;
+import java.awt.Font;
+import javax.swing.JSpinner;
+import javax.swing.SwingConstants;
 
 public class Peliculas extends JFrame {
 
@@ -32,11 +41,42 @@ public class Peliculas extends JFrame {
 	 */
 	public Peliculas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(950, 600);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(31, 150, 251, 252);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(343, 150, 251, 252);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(655, 150, 251, 252);
+		contentPane.add(lblNewLabel_2);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(917, 0, 17, 561);
+		contentPane.add(scrollBar);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setEditable(true);
+		comboBox.setBounds(10, 42, 133, 20);
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel_3 = new JLabel("Filtros");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(44, 14, 57, 20);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setBounds(754, 14, 152, 64);
+		contentPane.add(lblNewLabel_4);
 	}
-
 }
