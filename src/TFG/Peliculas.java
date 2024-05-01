5d954d350b8019d8ccffe31e1bc0a333f9b5cd74
antoonioaraujo <ajaraujoplata@gmail.com>
2024-05-01 12:14:14 +0200

    fotitos god en botoncitos
    yituiiyiyiyiyuiiyi

--- src/TFG/Peliculas.java
+++ src/TFG/Peliculas.java
@@ -34,7 +34,7 @@ public class Peliculas extends JFrame {
         setSize(950, 600);
         setLocationRelativeTo(null);
         contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setBorder(new EmptyBorder(100, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
 		
@@ -43,22 +43,33 @@ public class Peliculas extends JFrame {
 		contentPane.add(lblNewLabel_4);
         // Creamos un JPanel que representará el contenido principal
         JPanel mainContentPanel = new JPanel();
-        mainContentPanel.setLayout(new GridLayout(3, 3, 15, 15));
-        mainContentPanel.setBounds(50, 50, 800, 400);// Establecemos un tamaño preferido para el JPanel
-
+        mainContentPanel.setLayout(new GridLayout(3, 3, 0, 50));
+        
         // Creamos un JScrollPane y le agregamos el JPanel principal como contenido
         JScrollPane scrollPane = new JScrollPane(mainContentPanel);
+        scrollPane.setLocation(5, 4);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         
         JButton btnNewButton = new JButton("New button");
-        btnNewButton.setBounds(25, 180, 251, 252);
+        ImageIcon icon = new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\TFG\\src\\Imagenes\\SpiderMan3.jpeg");
+        Image image = icon.getImage().getScaledInstance(333, 433, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(image);
+        btnNewButton.setIcon(scaledIcon);
         mainContentPanel.add(btnNewButton);
         
+        
         JButton btnNewButton_1 = new JButton("New button");
-        btnNewButton_1.setBounds(325, 180, 251, 252);
+        ImageIcon icon1 = new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\TFG\\src\\Imagenes\\AtrapameSiPuedes.jpeg");
+        Image image1 = icon1.getImage().getScaledInstance(333, 433, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon1 = new ImageIcon(image1);
+        btnNewButton_1.setIcon(scaledIcon1);
         mainContentPanel.add(btnNewButton_1);
         
         JButton btnNewButton_2 = new JButton("New button");
-        btnNewButton_2.setBounds(625, 180, 251, 252);
+        ImageIcon icon2 = new ImageIcon("C:\\Users\\Usuario\\Documents\\GitHub\\TFG\\src\\Imagenes\\BeauTieneMiedojpeg.jpeg");
+        Image image2 = icon2.getImage().getScaledInstance(333, 433, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon2 = new ImageIcon(image2);
+        btnNewButton_2.setIcon(scaledIcon2);
         mainContentPanel.add(btnNewButton_2);
         
         JComboBox comboBox = new JComboBox();
@@ -71,37 +82,30 @@ public class Peliculas extends JFrame {
         lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
         
         JButton btnNewButton_3 = new JButton("New button");
-        btnNewButton_3.setBounds(25, 480, 251, 252);
         mainContentPanel.add(btnNewButton_3);
         
         JButton btnNewButton_4 = new JButton("New button");
-        btnNewButton_4.setBounds(325, 480, 251, 252);
         mainContentPanel.add(btnNewButton_4);
         
         JButton btnNewButton_5 = new JButton("New button");
-        btnNewButton_5.setBounds(625, 480, 251, 252);
         mainContentPanel.add(btnNewButton_5);
         
         JButton btnNewButton_6 = new JButton("New button");
-        btnNewButton_6.setBounds(25, 780, 251, 252);
         mainContentPanel.add(btnNewButton_6);
         
         JButton btnNewButton_7 = new JButton("New button");
-        btnNewButton_7.setBounds(325, 780, 251, 252);
         mainContentPanel.add(btnNewButton_7);
         
         JButton btnNewButton_8 = new JButton("New button");
-        btnNewButton_8.setBounds(625, 780, 251, 252);
         mainContentPanel.add(btnNewButton_8);
         
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBounds(0, 5, 950, 600); // Establecemos el tamaño y posición del JScrollPane
 
         // Agregamos el JScrollPane al contentPane sin reemplazar componentes existentes
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(scrollPane, BorderLayout.CENTER);
         getContentPane().revalidate();
-        mainContentPanel.setPreferredSize(new Dimension(950, 1100));
+        mainContentPanel.setPreferredSize(new Dimension(850, 1400));
         contentPane.add(scrollPane);
 	}
 }
