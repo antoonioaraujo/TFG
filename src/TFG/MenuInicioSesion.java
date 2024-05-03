4594393a578f88411a84ccdb7bbd9a2e83be8ca1
antoonioaraujo <ajaraujoplata@gmail.com>
2024-05-03 10:58:49 +0200

    l
    l

--- src/TFG/MenuInicioSesion.java
+++ src/TFG/MenuInicioSesion.java
@@ -11,6 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 import java.awt.*;
@@ -26,7 +27,6 @@ public class MenuInicioSesion extends JFrame {
 	 * Launch the application.
 	 */
 
-
 	/**
 	 * Create the frame.
 	 */
@@ -41,59 +41,95 @@ public class MenuInicioSesion extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		UsuarioF = new JTextField();
 		UsuarioF.setBounds(460, 170, 96, 19);
 		contentPane.add(UsuarioF);
 		UsuarioF.setColumns(10);
-		
+
 		JLabel LoginT = new JLabel("Inicio de Sesion");
 		LoginT.setFont(new Font("Tahoma", Font.PLAIN, 27));
 		LoginT.setHorizontalAlignment(SwingConstants.CENTER);
 		LoginT.setBounds(375, 87, 200, 35);
 		contentPane.add(LoginT);
-		
+
 		JLabel UsuarioT = new JLabel("Usuario");
 		UsuarioT.setHorizontalAlignment(SwingConstants.CENTER);
 		UsuarioT.setBounds(350, 170, 45, 13);
 		contentPane.add(UsuarioT);
-		
+
 		JLabel ContraseñaT = new JLabel("Contraseña");
 		ContraseñaT.setHorizontalAlignment(SwingConstants.CENTER);
 		ContraseñaT.setBounds(350, 220, 69, 13);
 		contentPane.add(ContraseñaT);
-		
+
 		ContraseñaF = new JTextField();
 		ContraseñaF.setBounds(460, 220, 96, 19);
 		contentPane.add(ContraseñaF);
 		ContraseñaF.setColumns(10);
-		
+
 		JLabel ResgitroT = new JLabel("¿No tienes cuenta? Registrate");
 		ResgitroT.setHorizontalAlignment(SwingConstants.CENTER);
 		ResgitroT.setBounds(292, 300, 182, 46);
 		contentPane.add(ResgitroT);
-		
+
 		JButton RegistroB = new JButton("Registrarte");
 		RegistroB.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				MenuRegistro MenuRegistro = new MenuRegistro();
 				MenuRegistro.setVisible(true);
 				dispose();
-				
+
 			}
 		});
 		RegistroB.setBounds(327, 370, 100, 27);
 		contentPane.add(RegistroB);
-		
-		JButton ContiuarB = new JButton("Continuar");
-		ContiuarB.addActionListener(new ActionListener() {
+
+		JButton ContinuarB = new JButton("Continuar");
+		ContinuarB.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
+				ConexionMySQL conexion = new ConexionMySQL("freedb_Persian", "H!!$t822$tVdcEt", "freedb_Persian");
+				try {
+					// Conexion
+					conexion.conectar();
+					System.out.println("Conectado");
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				// Iniciar sesion en el usuario verificando que el usuario y la contraseña sean
+				// correctos
+				try {
+					String consulta = "SELECT * FROM registro WHERE usuario='" + UsuarioF.getText()
+							+ "' AND contrasena='" + ContraseñaF.getText() + "'";
+					conexion.ejecutarSelect(consulta);
+					System.out.println("Sesion iniciada con exito");
+
+				} catch (Exception e2) {
+					// TODO: handle exception
+				}
+				// Desconexion
+				try {
+					conexion.desconectar();
+					System.out.println("Desconectado");
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				// Vaciar campos
+				UsuarioF.setText("");
+				ContraseñaF.setText("");
+
+				// Abrir la ventana principal
+				Peliculas Peliculas = new Peliculas();
+				Peliculas.setVisible(true);
+				dispose();
 			}
 		});
-		ContiuarB.setBounds(482, 370, 100, 27);
-		contentPane.add(ContiuarB);
-		
-		
+		ContinuarB.setBounds(482, 370, 100, 27);
+		contentPane.add(ContinuarB);
 
 	}
 }
